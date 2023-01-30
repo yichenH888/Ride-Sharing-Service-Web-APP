94a022b4c18ab9bef0352bd5e00e2bbb2e94643d
rs590 <rs590@duke.edu>
2023-01-30 00:15:00 -0500

    working on home page & 403,404 page added

---
+++ web-app/apps/home/urls.py
@@ -0,0 +1,12 @@
+from apps.home import views
+from django.urls import path, re_path
+
+urlpatterns = [
+
+    # The home page
+    path('', views.index, name='home'),
+
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
+
+]
